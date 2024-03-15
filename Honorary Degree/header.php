40cb01511e7b6e8ca12a69dd79ba2a413d25546c
kchase9 <kristeen.a.chase@gmail.com>
2024-03-15 15:07:51 -0400

    Switched to PHP, not included

---
+++ Honorary Degree/header.php
@@ -0,0 +1,45 @@
+
+<!doctype html>
+<html lang="en">
+    <style>
+        body {
+            margin-bottom: -60px;
+        }  
+    </style>
+  <body> 
+
+    <nav class="navbar navbar-expand-lg"style="background-color: #0000;" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a href="https://uog.edu.gy" class="navbar-brand">
+                <img src="img/ug-logo.png" width="48" height="48" style="border-radius: 60px;">
+            </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home</a>
+              </li>
+              <li class="nav-item">
+                <a href="https://uog.edu.gy/nomination-honorary-degree" class="nav-link">Nominate</a>
+            </li>
+              <li class="nav-item dropdown dropdown-left">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Our Associates
+                </a>
+                <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="https://uog.edu.gy">University of Guyana</a></li>
+                    <li><a class="dropdown-item" href="https://paceuog.wixsite.com/pace-uog/">UG PACE Office</a></li>
+                    <li><a class="dropdown-item" href="https://ugalumnipride.com">UG Alumni Association</a></li>
+                    <li><a class="dropdown-item" href="https://ugfoundationinc.com">UG Foundation Inc.</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+    </nav>
+
+    
+  </body>
+</html>
