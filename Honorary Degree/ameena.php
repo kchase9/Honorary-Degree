ffb52a18f76c5715b1404d8ce29dcd9d720f9c40
kchase9 <kristeen.a.chase@gmail.com>
2024-10-25 22:51:08 -0400

    ameena

--- Honorary Degree/ameena.php
+++ Honorary Degree/ameena.php
@@ -28,68 +28,58 @@
 <body>
     <div class="rec">
 
-    <div class="intro w-100" style="background-image: url('img/ameena-gafoor.jpg');">
-        <div class="container">
-            <div class="col-lg-6 col-md-12 ">
-                <div class="align-items-start d-flex mb-3 flex-column">
-                    <h1 class="p-3">Ameena Gafoor</h1>
-                    <h2 class="p-3">Doctorate of Letters</h2>
-                    <h3 class="p-3">Date: 9th December, 2022</h3>
-                    <p class="p-3">Ameena Gafoor, born in 1941, was a distinguished philanthropist, cultural advocate, and literary patron in Guyana, known for her dedication to promoting the arts and education. Rising from humble beginnings, Gafoor played a transformative role as Secretary of the Arts Forum Inc. and Executive Editor of the Caribbean Review of Books, championing underrepresented voices in literature. She founded the Ameena Gafoor Institute, published The Arts Journal, and authored key works, including the first major study of Roy Heath’s fiction and her memoir, Lantern in the Wind (2020), which illuminates aspects of Muslim life in Guyana. Her influence extended through teaching positions at Central and Tutorial High Schools, as well as through the Gafoor Foundation, which she co-founded to support medical, educational, and sports initiatives, including the Ameena Gafoor Medical and Dialysis Centre. Honored with multiple awards, including the National Medal of Service, Gafoor’s legacy reached globally with the founding of the Ameena Gafoor Institute for the Study of Indentureship and its Legacies at Cambridge University. President Dr. Irfaan Ali celebrated her life’s work, noting her kindness, humility, and profound impact on Guyana’s cultural landscape.
-                    </p>
-                    <div class="">
-                        <a class="btn" href="index.php"><i class="fa-solid fa-house" style="background-color:none;"></i>  Home</a>
-                        <a href="#h-gal" class="btn">See More</a>
+    <div class="intro" >
+        <div class="">
+            <div class="row w-100">
+                <div class="col-md-6 main-pic" style="background-image: url('img/Ameena Gafoor1.jpg');"></div>
+                <div class="col-md-6 col-sm-12 ">
+                    <div class="align-items-right bg-green" id="bio-preview">
+                        <h1 class="p-3">Hon. Dr. Ameena Gafoor</h1>
+                        <p class="p-3">Ameena Gafoor, born in 1941, was a distinguished philanthropist, cultural advocate, and literary patron in Guyana, known for her dedication to promoting the arts and education. Rising from humble beginnings, Gafoor played a transformative role as Secretary of the Arts Forum Inc. and Executive Editor of the Caribbean Review of Books, championing underrepresented voices in literature. She founded the Ameena Gafoor Institute, published The Arts Journal, and authored key works, including the first major study of Roy Heath’s fiction and her memoir, Lantern in the Wind (2020), which illuminates aspects of Muslim life in Guyana. Her influence extended through teaching positions at Central and Tutorial High Schools, as well as through the Gafoor Foundation, which she co-founded to support medical, educational, and sports initiatives, including the Ameena Gafoor Medical and Dialysis Centre. Honored with multiple awards, including the National Medal of Service, Gafoor’s legacy reached globally with the founding of the Ameena Gafoor Institute for the Study of Indentureship and its Legacies at Cambridge University. President Dr. Irfaan Ali celebrated her life’s work, noting her kindness, humility, and profound impact on Guyana’s cultural landscape.
+                        </p>
+                        <div class="btn-content">
+                            <a class="btn" href="index.php"><i class="fa-solid fa-house" style="background-color:none;"></i>  Home</a>
+                            <a href="#h-gal" class="btn">See More</a>
+                        </div>
                     </div>
+                    
                 </div>
+
                 
             </div>
-
-            
         </div>
+        
     
     </div>
     
-    <div class="container">
-        <div id="gallery">
-            <div class="text-lg-end">
-                <h2 id="h-gal">Gallery</h2>
-                <hr>
-            </div>
+    <div class="content">
+        <div id="page-heading">
+            <h2 id="h-gal">Conferral of an Honorary Doctorate of Laws on Justice Hon Dr Ameena Gafoor</h2>
+            <hr>
         </div>
 
-        <!--  Image Gallery, adapted from Bootstrap -->
+        <div class="video-container">
+            <iframe width="1236" height="695" src="https://www.youtube.com/embed/pZUENP504Bg" title="Reading of Dr Ameena Gafoors Citation | University of Guyana&#39;s  56th Convocation Ceremony 2022" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        </div>
+
+        <div id="page-heading">
+            <h2 id="h-gal">Highlights from the Robing of Hon Dr Ameena Gafoor</h2>
+            <hr>
+        </div>
 
         <div class="row">
-            <div class="col-lg-12 d-flex justify-content-center align-self-center">
-                <div id="img-carousel" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <!-- Those buttons below -->
-                    <button type="button" data-bs-target="#img-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#img-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+            <div class="col-lg-12">
+                <div class="masonry-container">
+                    <div class="masonry-item">
+                        <img src="img/Ameena Gafoor1.jpg" class="img-fluid">
                     </div>
-
-                    <!-- Images -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="img/Ameena Gafoor1.jpg" class="d-block">
-                        </div>    
-                        <div class="carousel-item">
-                            <img src="img/Ameena Gafoor2.jpg" class="d-block">
-                        </div>
+                    <div class="masonry-item">
+                        <img src="img/Ameena Gafoor2.jpg" class="img-fluid">
                     </div>
-                    <!--  Controls -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#img-carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#img-carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
         </div>
+
         
             
 
