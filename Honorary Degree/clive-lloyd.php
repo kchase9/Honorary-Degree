4da68cc50ed3762a53f7d19b45a05cfbe3048fb0
kchase9 <kristeen.a.chase@gmail.com>
2024-11-05 13:10:45 -0400

    clive done

--- Honorary Degree/clive-lloyd.php
+++ Honorary Degree/clive-lloyd.php
@@ -26,66 +26,49 @@
 </head>
 <body>
     <div class="rec">
-    <div class="intro w-100" style="background-image: url('img/clive-lloyd.jpeg');">
-        <div class="container">
-            <div class="col-lg-6 col-md-12 ">
-                <div class="align-items-start d-flex mb-3 flex-column">
+    <div class="intro">
+        <div class="">
+            <div class="row w-100">
+                <div class="col-md-6 main-pic" style="background-image: url('img/Sir Clive Lloyd.jpg');">
+                </div>
+
+                <div class="col-md-6 col-sm-12 ">
+                <div class="align-items-right bg-green" id="bio-preview">
                     <h1 class="p-3">Clive Lloyd</h1>
-                    <h2 class="p-3">Doctorate of Letters</h2>
-                    <h3 class="p-3">Date: 9th December, 2022</h3>
                     <p class="p-3">Born in Queenstown, Georgetown, Lloyd is celebrated as one of the most successful West Indian cricket captains, leading his team to an unmatched 27-game unbeaten streak and consecutive Cricket World Cup victories in 1975 and 1979. Lloyd's accolades include being appointed a Commander of the Order of the British Empire (CBE) for his services to the sport. Beyond his cricketing career, he has contributed as a coach and served with the International Cricket Council. In his acceptance speech, Lloyd advocated for addressing societal issues, including domestic abuse and racism, and highlighted the need for the development of sports facilities, urging upgrades to the Providence Stadium as a tribute to Caribbean sports legends.</p>
-                    <div class="">
+                    <div class="btn-content">
                         <a class="btn" href="index.php"><i class="fa-solid fa-house" style="background-color:none;"></i>  Home</a>
                         <a href="#h-gal" class="btn">See More</a>
                     </div>
                 </div>
                 
             </div>
+            </div>
+            
 
             
         </div>
     
     </div>
     
-    <div class="container">
-        <div id="gallery">
-            <div class="text-lg-end">
-                <h2 id="h-gal">Gallery</h2>
+    <div class="content">
+        <div id="page-heading">
+                <h2 id="h-gal">Highlights from the Robing of Hon Dr Clive Lloyd</h2>
                 <hr>
             </div>
-        </div>
 
-        <!--  Image Gallery, adapted from Bootstrap -->
-
-        <div class="row">
-            <div class="col-lg-12 d-flex justify-content-center align-self-center">
-                <div id="img-carousel" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <!-- Those buttons below -->
-                    <button type="button" data-bs-target="#img-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="masonry-container">
+                        <div class="masonry-item ">
+                            <img src="img/Sir Clive Lloyd.jpg" class="img-fluid">
+                        </div>
                     </div>
-
-                    <!-- Images -->
-                    <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="img/Sir Clive Lloyd.jpg" class="d-block">
-                    </div>
-                    </div>
-                    <!--  Controls -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#img-carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#img-carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
-        </div>
-        
-            
 
+
+        </div>
     </div>
 </div>
     
