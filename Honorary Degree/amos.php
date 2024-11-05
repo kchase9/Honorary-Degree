fd3bb919b306cfb682145b87470f97a8a0296bf7
kchase9 <kristeen.a.chase@gmail.com>
2024-11-05 12:07:58 -0400

    baroness valerie amos completed

--- Honorary Degree/amos.php
+++ Honorary Degree/amos.php
@@ -27,69 +27,48 @@
 <body>
     <div class="rec">
 
-    <div class="intro w-100" style="background-image: url('img/baroness-amos.jpg');">
-        <div class="container">
-            <div class="col-lg-6 col-md-12 ">
-                <div class="align-items-start d-flex mb-3 flex-column">
+    <div class="intro">
+        <div class="">
+            <div class="row w-100">
+                <div class="col-md-6 main-pic" style="background-image: url('img/Univ_Baroness_Amos_by_John_Cairns_12.1.23-106 (1).jpg'); ">
+            </div>
+            <div class="col-md-6 col-sm-12 ">
+                <div class="align-items-right bg-green" id="bio-preview">
                     <h1 class="p-3">Baroness Valerie Amos</h1>
-                    <h2 class="p-3">Doctorate of Laws</h2>
-                    <h3 class="p-3">Date: 10th November, 2023</h3>
                     <p class="p-3">Baroness Valerie Amos, born in British Guiana (now Guyana), is a distinguished leader in public service, education, and advocacy for diversity and equality. Recently named a Lady Companion of the Most Noble Order of the Garter, she is the first person of color to receive this prestigious honor. Amos broke multiple barriers throughout her career, including becoming the first black woman to serve in a British Cabinet and as Leader of the House of Lords (2003-2007), the UN Under-Secretary-General for Humanitarian Affairs, British High Commissioner to Australia, and the first black head of both SOAS and University College Oxford. Passionate about inclusive education, she has championed initiatives that increase diversity within academia and advocate for representation, helping reshape educational cultures to be more welcoming to all students. Baroness Amos will soon receive an Honorary Doctor of Laws from the University of Guyana, marking yet another milestone in her career dedicated to public service and global leadership.
                     </p>
-                    <div class="">
+                    <div class="btn-content">
                         <a class="btn" href="index.php"><i class="fa-solid fa-house" style="background-color:none;"></i>  Home</a>
                         <a href="#h-gal" class="btn">See More</a>
                     </div>
                 </div>
-                
-            </div>
 
+                </div>
+            </div>
             
         </div>
     
     </div>
     
-    <div class="container">
-        <div id="gallery">
-            <div class="text-lg-end">
-                <h2 id="h-gal">Gallery</h2>
-                <hr>
-            </div>
+    <div class="content">
+        <div id="page-heading">
+            <h2 id="h-gal">The Eloquent Baroness Valerie Amos</h2>
+            <hr>
         </div>
 
-        <!--  Image Gallery, adapted from Bootstrap -->
-
-        <div class="row">
-            <div class="col-lg-12 d-flex justify-content-center align-self-center">
-                <div id="img-carousel" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <!-- Those buttons below -->
-                    <button type="button" data-bs-target="#img-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                    <button type="button" data-bs-target="#img-carousel" data-bs-slide-to="1" class="active" aria-current="true" aria-label="Slide 2"></button>
+        <!--  Image Gallerie lery, adapted from Bootstrap -->
+          <div class="row">
+            <div class="col-lg-12">
+                <div class="masonry-container">
+                    <div class="masonry-item">
+                        <img src="img/Univ_Baroness_Amos_by_John_Cairns_12.1.23-106 (1).jpg" class="img-fluid">
                     </div>
-
-                    <!-- Images -->
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="img/Univ_Baroness_Amos_by_John_Cairns_12.1.23-106 (1).jpg" class="d-block">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="img\Baroness Valerie Ann Amos.jpg" class="d-block">
-                        </div>
+                    <div class="masonry-item">
+                        <img src="img\Baroness Valerie Ann Amos.jpg" class="img-fluid">
                     </div>
-                    <!--  Controls -->
-                    <button class="carousel-control-prev" type="button" data-bs-target="#img-carousel" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#img-carousel" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                    </button>
                 </div>
             </div>
-        </div>
-        
+          </div>      
             
 
     </div>
